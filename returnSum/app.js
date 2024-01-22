e353c2a5efd2a673b219f1747cf9695ea2df00f2
User <Willspri@gmail.com>
2024-01-22 15:53:46 +1300

    Add project files.

---
+++ returnSum/app.js
@@ -0,0 +1,8 @@
+'use strict';
+
+function addition(a, b) {
+    return a + b;
+}
+//assign 0 and 1 index to a and b and log the results of the function to the console
+console.log(addition(3, 2));    
+  
