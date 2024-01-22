6896bd4027a11176bbf725fed1d9514a4b04b410
User <Willspri@gmail.com>
2024-01-22 15:54:13 +1300

    complete

--- returnSum/app.js
+++ returnSum/app.js
@@ -4,5 +4,4 @@ function addition(a, b) {
     return a + b;
 }
 //assign 0 and 1 index to a and b and log the results of the function to the console
-console.log(addition(3, 2));    
-  
+console.log(addition(3, 2));
